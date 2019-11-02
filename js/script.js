efb9cba844b2bd6c69b114e8cc553f76532d0824
devjosegomez <devjosegomez@gmail.com>
2019-11-02 09:22:58 -0600

    adding classes to animate the dice

--- js/script.js
+++ js/script.js
@@ -1,17 +1,37 @@
 const output = document.querySelector("#output");
 const button = document.querySelector("button");
+const audio = document.querySelector("#diceSound");
+const gameArea = document.querySelector("#gameArea");
+let lastRoll;
 //Dice's faces
 let dice = [[5], [3,7], [3,5,7], [1,3,7,9], [1,3,5,7,9], [1,3,4,6,7,9]];
             //1,    2,      3   ,   4     ,     5       ,       6
 
 button.addEventListener("click",() =>{
+    console.log("Last roll: " + lastRoll);
+    button.disabled = true;
+    gameArea.classList.add("rubberBand");
+
+    setTimeout(function(){
+        button.disabled = false;
+        if(gameArea.classList[2]){
+            gameArea.classList.remove(gameArea.classList[2]);
+        }
+    }, 900);
+    audio.playbackRate = 1.4;
+    audio.play();
+
     let rollDice = roll(6);
+    if(rollDice == lastRoll){
+        rollDice = roll(6);
+    }
     console.log(rollDice);
     let diceHolder = builder(rollDice);
     if(output.children[0]){
         output.children[0].remove();
     }
     output.appendChild(diceHolder);
+    lastRoll = rollDice;
 });
 
 function roll(n){
