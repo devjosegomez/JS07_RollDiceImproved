f92e399675f720b33dadd335beaa6750b5d618d9
devjosegomez <devjosegomez@gmail.com>
2019-11-02 08:19:03 -0600

    Added dots to the dice container

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const output = document.querySelector("#output");
 const button = document.querySelector("button");
+//Dice's faces
+let dice = [[5], [3,7], [3,5,7], [1,3,7,9], [1,3,5,7,9], [1,3,4,6,7,9]];
+            //1,    2,      3   ,   4     ,     5       ,       6
 
 button.addEventListener("click",() =>{
     let rollDice = roll(6);
@@ -18,6 +21,19 @@ function roll(n){
 
 function builder(n){
     let diceDiv = document.createElement("div");
+    let fillDice = dice[n-1];
+    console.log(fillDice);
+    //Adding class to display dots
+    for(let i=1; i<10; i++){
+        let divDot = document.createElement("div");
+        divDot.setAttribute("class", "dot");
+        if(fillDice.includes(i)){
+            divDot.classList.add("black");
+        }
+        diceDiv.appendChild(divDot);
+    }
+    
     diceDiv.setAttribute("class", "dicer");
+    
     return diceDiv;
 }
